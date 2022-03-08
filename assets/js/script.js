f90533510b7d9dcdb6db12383c01394cf78c95fb
Anastasiia-Ciloci <anastasiiaciloci@gmail.com>
2022-03-08 14:09:59 -0500

    added files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 console.log("here");
+
+//web imdb: https://imdb-api.com/Identity/Account/ConfirmEmail?userId=c0873f32-a043-4490-8a0d-2186c973f29e&code=Q2ZESjhCMVBWRzREQW1GUHZETy92bjJCanZwNjZ0bWJmdTlCSEVJWDJ4MmNZK21kRFpCcUMrQXJJL1BzWUtpaUY1UngwRTBQVzM3aFVnSFlUc0xVMi9rOUFUczhscFlxOU1xRVhXaFR5TUdxY2Z1UUo5aEN1MVl0TGFwV09Bc1NJbW9NdGlJVHk0SmZOcEtLQXdLN0dSTTByNk14Q3FMcVpuTEg1dWFlQVEvcGZwSThSTXNta0kwRnJVeTFBT2dVRGJnMFk4eDlaUVlyd1AyYWNSNmJrclQxczZaNldUdXIyQnd1blRBV2FtWnNucis5QXJvZnJ2VkdsZmZiU3RSVmJBMmRTdz09
+
+//apikey: k_iblvk1h1
+
+//api omdbapi: http://www.omdbapi.com/?i=tt3896198&apikey=9add4f79
